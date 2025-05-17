b9d3231c0542a4450e0f6b6933563a0b7bc7afe7
gkiri <hkkiri@gmail.com>
2025-05-17 17:12:11 +0200

    Update run_500m_server.sh

--- smolVLM_workspace/run_500m_server.sh
+++ smolVLM_workspace/run_500m_server.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+./llama-server \
+  -m "/mnt/c/Users/MY PC/Desktop/SmolVLM-500M-Instruct-Q8_0.gguf" \
+  --mmproj "C:\\Users\\MY PC\\Desktop\\project_VLM\\mmproj-SmolVLM-500M-Instruct-Q8_0.gguf"
